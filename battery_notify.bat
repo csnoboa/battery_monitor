6e319c4c06325e20e3371c2d1c30d793842efca8
csnoboa <caique2noboa@gmail.com>
2021-03-19 10:01:14 -0300

    adding the bat and config

---
+++ battery_notify.bat
@@ -0,0 +1,1 @@
+python C:\scr\python\main.py
--- main.py
+++ main.py
@@ -1,29 +1,39 @@
 import psutil
 from win10toast import ToastNotifier
 import time
+import json
 
-while True:
+def run(configuration):
+    while True:
+        min_bat = configuration["min_battery"]
+        max_bat = configuration["max_battery"]
+        sleep_time = configuration["time_check"]
 
-    min_bat = 35
-    max_bat = 75
-    sleep_time = 60
+        battery = psutil.sensors_battery()
 
-    battery = psutil.sensors_battery()
+        plugged = battery.power_plugged
+        
+        percent = battery.percent
+            
+        toaster = ToastNotifier()
+            
+        if percent <= min_bat and not plugged:
+            toaster.show_toast("Plug the Battery Now!","The percent of the battery is: " + str(percent) + "% !!!")
+            
+        elif percent >= max_bat and plugged:
+            toaster.show_toast("Unplug the Battery Now!","The percent of the battery is: " + str(percent) + "% !!!")
+            
+        time.sleep(sleep_time)
+        
 
-    plugged = battery.power_plugged
-    
-    percent = battery.percent
+def load_config():
+    data = json.load(open('config.json'))
+    return data
         
-    toaster = ToastNotifier()
-        
-    if percent <= min_bat and not plugged:
-        toaster.show_toast("Plug the Battery Now!","The percent of the battery is: " + str(percent) + "% !!!")
-        
-    elif percent >= max_bat and plugged:
-        toaster.show_toast("Unplug the Battery Now!","The percent of the battery is: " + str(percent) + "% !!!")
-        
-    time.sleep(sleep_time)
-    
-    
+
+if __name__ == '__main__':
+    configuration = load_config()
+    print(configuration)
+    run(configuration)
     
 
