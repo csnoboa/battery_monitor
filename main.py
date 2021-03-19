f874c2010b3a28600f883e706a62cc717bdcf5b2
csnoboa <caique2noboa@gmail.com>
2021-03-19 09:43:48 -0300

    change to psutil

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
-import sys
+import psutil
+from win10toast import ToastNotifier
+import time
 
-params = read_apm()
-ac = params['ac']
-charge = params['charge'] # guarda o valor da carga da bateria
+while True:
 
-print(charge)
+    min_bat = 35
+    max_bat = 75
+    sleep_time = 60
 
+    battery = psutil.sensors_battery()
+
+    plugged = battery.power_plugged
+    
+    percent = battery.percent
+        
+    toaster = ToastNotifier()
+        
+    if percent <= min_bat and not plugged:
+        toaster.show_toast("Plug the Battery Now!","The percent of the battery is: " + str(percent) + "% !!!")
+        
+    elif percent >= max_bat and plugged:
+        toaster.show_toast("Unplug the Battery Now!","The percent of the battery is: " + str(percent) + "% !!!")
+        
+    time.sleep(sleep_time)
+    
+    
+    
 
